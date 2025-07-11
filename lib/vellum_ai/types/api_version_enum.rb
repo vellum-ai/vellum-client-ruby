ec58f0233d7f82318896ab2b986fc561418bc76c
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-07-11 00:13:54 +0000

    SDK regeneration

--- lib/vellum_ai/types/api_version_enum.rb
+++ lib/vellum_ai/types/api_version_enum.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
 module Vellum
-  API_VERSION_ENUM = String
+# * `2024-10-25` - V2024_10_25
+#  * `2025-07-30` - V2025_07_30
+  class ApiVersionEnum
+
+    TWO_THOUSAND_TWENTY_FOUR_1025 = "2024-10-25"
+    TWO_THOUSAND_TWENTY_FIVE_0730 = "2025-07-30"
+
+  end
 end
