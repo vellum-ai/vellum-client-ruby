f6ad79a8e654f6542f5263af5f7dabaa3dedcee9
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-07-22 16:43:24 +0000

    SDK regeneration

--- lib/vellum_ai/types/quota.rb
+++ lib/vellum_ai/types/quota.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
+require_relative "name_enum"
 require "ostruct"
 require "json"
 
 module Vellum
   class Quota
-  # @return [String] 
+  # @return [Vellum::NameEnum] 
     attr_reader :name
   # @return [Integer] 
     attr_reader :value
@@ -18,7 +19,7 @@ module Vellum
 
     OMIT = Object.new
 
-    # @param name [String] 
+    # @param name [Vellum::NameEnum] 
     # @param value [Integer] 
     # @param period_seconds [Integer] 
     # @param additional_properties [OpenStruct] Additional properties unmapped to the current class definition
@@ -62,7 +63,7 @@ end
     # @param obj [Object] 
     # @return [Void]
     def self.validate_raw(obj:)
-      obj.name.is_a?(String) != false || raise("Passed value for field obj.name is not the expected type, validation failed.")
+      obj.name.is_a?(Vellum::NameEnum) != false || raise("Passed value for field obj.name is not the expected type, validation failed.")
       obj.value&.is_a?(Integer) != false || raise("Passed value for field obj.value is not the expected type, validation failed.")
       obj.period_seconds&.is_a?(Integer) != false || raise("Passed value for field obj.period_seconds is not the expected type, validation failed.")
     end
