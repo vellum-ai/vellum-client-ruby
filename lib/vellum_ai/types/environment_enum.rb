9b19950332bc767f1abde5e5738dc5e1caf38794
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-08-27 20:54:14 +0000

    SDK regeneration

--- lib/vellum_ai/types/environment_enum.rb
+++ lib/vellum_ai/types/environment_enum.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 module Vellum
-# * `DEVELOPMENT` - Development
-#  * `STAGING` - Staging
-#  * `PRODUCTION` - Production
   class EnvironmentEnum
 
     DEVELOPMENT = "DEVELOPMENT"
