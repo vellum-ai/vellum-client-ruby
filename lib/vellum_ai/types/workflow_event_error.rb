57788ade76375448a35e18cb1bd39395b0470e4e
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-09-09 20:42:49 +0000

    SDK regeneration

--- lib/vellum_ai/types/workflow_event_error.rb
+++ lib/vellum_ai/types/workflow_event_error.rb
@@ -9,6 +9,8 @@ module Vellum
     attr_reader :message
   # @return [Vellum::WorkflowExecutionEventErrorCode] 
     attr_reader :code
+  # @return [String] 
+    attr_reader :stacktrace
   # @return [OpenStruct] Additional properties unmapped to the current class definition
     attr_reader :additional_properties
   # @return [Object] 
@@ -19,13 +21,17 @@ module Vellum
 
     # @param message [String] 
     # @param code [Vellum::WorkflowExecutionEventErrorCode] 
+    # @param stacktrace [String] 
     # @param additional_properties [OpenStruct] Additional properties unmapped to the current class definition
     # @return [Vellum::WorkflowEventError]
-    def initialize(message:, code:, additional_properties: nil)
+    def initialize(message:, code:, stacktrace: OMIT, additional_properties: nil)
       @message = message
       @code = code
+      @stacktrace = stacktrace if stacktrace != OMIT
       @additional_properties = additional_properties
-      @_field_set = { "message": message, "code": code }
+      @_field_set = { "message": message, "code": code, "stacktrace": stacktrace }.reject do | _k, v |
+  v == OMIT
+end
     end
 # Deserialize a JSON object to an instance of WorkflowEventError
     #
@@ -36,9 +42,11 @@ module Vellum
       parsed_json = JSON.parse(json_object)
       message = parsed_json["message"]
       code = parsed_json["code"]
+      stacktrace = parsed_json["stacktrace"]
       new(
         message: message,
         code: code,
+        stacktrace: stacktrace,
         additional_properties: struct
       )
     end
@@ -57,6 +65,7 @@ module Vellum
     def self.validate_raw(obj:)
       obj.message.is_a?(String) != false || raise("Passed value for field obj.message is not the expected type, validation failed.")
       obj.code.is_a?(Vellum::WorkflowExecutionEventErrorCode) != false || raise("Passed value for field obj.code is not the expected type, validation failed.")
+      obj.stacktrace&.is_a?(String) != false || raise("Passed value for field obj.stacktrace is not the expected type, validation failed.")
     end
   end
 end
